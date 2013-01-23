d4f434edef0d83b709a98fad2375f5cf1c966aa5
Ryan Abel <abelr@mail.gvsu.edu>
2013-01-22 21:07:41 -0500

    Started work on simple shell.

---
+++ labs/lab_2/simple_shell.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main() {
+  char* line;
+  char* command;
+  char* flags;
+  char* quit = "quit";
+  char* fullCommand[3];
+  int status, x;
+  x = 1; 
+
+  while(x == 1) {
+    pid_t pid, child;
+
+    printf("Prompt: ");
+    fgets(line, 100, stdin);
+    command = strtok(line, " ");
+    flags = strtok(NULL, " ");
+
+    command[strlen(command) - 1] = '\0';
+
+    fullCommand[0] = command;
+    fullCommand[1] = flags;
+    fullCommand[2] = NULL;
+
+    if(strcmp(quit, command) == 0){
+      puts("Exiting shell...");
+      break;
+    }
+
+    if((pid = fork()) < 0) {
+      perror("fork failure");
+      exit(1);
+    }
+    else if (pid == 0) {
+      if(execvp(fullCommand[0], fullCommand) < 0) {
+        perror("Command execution failed.");
+        exit(1);
+      } else {
+        exit(status);
+      }
+    } else {
+      child = wait(&status);
+    }
+  }
+  return 0;
+}

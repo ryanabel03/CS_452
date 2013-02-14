a80ef32b748cfc3e9a88ff7fe066c17fed5acb69
Ryan Abel <abelr@mail.gvsu.edu>
2013-02-13 22:31:58 -0500

    Finished project 2 and design doc.

--- projects/project_1/pipes.c
+++ projects/project_1/pipes.c
@@ -3,42 +3,105 @@
 #include <unistd.h>
 #include <string.h>
 
-void sendServerAbort(int fd[5][2], char server[], int numServers) {
-    int k = 0;
-    while(k < numServers) {
-      close(fd[k][0]);
-      
-      write(fd[k][1], server, strlen(server) + 1);
-      fflush(stdout);
-      k++;
-    }
+/*
+ * Displays a prompt for user input
+ *
+ */
+void displayPrompt() {
+  printf("Prompt: ");
+  fflush(stdout);
 }
 
+
+/*
+ * Called from a server to create a child process
+ *
+ */
+void createGrandChild(int fd2[10][2], int activeProcs) {
+
+  //Listen
+  while(1) {
+    char fromParent[1024];
+    read(fd2[activeProcs][0], fromParent, sizeof(fromParent));
+
+    printf("Grand child received %s", fromParent);
+    fflush(stdout);
+
+    fromParent[strlen(fromParent) - 1] = '\0';
+
+    if(strstr(fromParent, "kill") != NULL) {
+      printf("Killing grandchild process\n");
+      fflush(stdout);
+      displayPrompt();
+      exit(0);  
+    }
+  }
+}
+
+/*
+ * Called from the process manager to send a command to all servers
+ *
+ */
+void sendCommandToChild(int fd[5][2], char server[], int numServer) {
+  int k = 0;
+
+  //Send to every server
+  while(k < numServer) {
+    close(fd[k][0]);
+
+    write(fd[k][1], server, strlen(server) + 1);
+    fflush(stdout);
+    k++;
+    sleep(1);
+  }
+}
+
+/*
+ * Called from a server to kill itself
+ *
+ */
 void abortServer(char* childName, char* serverToAbort) {
   if(strcmp(serverToAbort, childName) == 0) {
     printf("Aborting server %s...\n", childName);
     fflush(stdout);
+    displayPrompt();
     exit(0);
   }
 }
 
+/*
+ * Called from a server to abort one of it's children
+ *
+ */
 void abortGrandChild(int fd[10][2], int numActive, int numMin, char* childToAbort, char* serverName) {
   if(strcmp(serverName, childToAbort) == 0) {
-    if(numActive - 1 > numMin) {
+    if(numActive - 1 >= numMin) {
       char message[] = "kill";
       close(fd[numActive - 1][0]);
       write(fd[numActive - 1][1], message, strlen(message) + 1);
       fflush(stdout);
     } else {
       printf("Deleting child on %s will put it under minimum of %d\n", serverName, numMin);
+      fflush(stdout);
+      displayPrompt();
     }
   }
 }
 
-void displayPrompt() {
-  printf("Prompt: ");
-  fflush(stdout);
+/*
+ * Called from a server to print its status
+ *
+ */
+void printServerStatus(char* serverName, pid_t serverId, pid_t children[10], int childrenNum) {
+  printf("SERVER:\t %s (%d)\n", serverName, serverId); 
+  int k = 0;
+  while(k < childrenNum) {
+    printf("----- Child %d (%d)\n", k + 1, children[k]);
+    k++;
+  }
+  displayPrompt();
 }
+
 
 int main() {
   pid_t server[5];
@@ -81,17 +144,25 @@ int main() {
         flag[0] = strtok(NULL, " ");
 
         int m = 0;
+
+        //Pass arguments
         while(flag[m] != NULL) {
           m++;
           flag[m] = strtok(NULL, " ");
         }
 
+        //Set important variables
         minProcs = atoi(flag[0]);
         maxProcs = atoi(flag[1]);
         serverName = flag[2];
         serverName[strlen(serverName) - 1] = '\0';
         child_index = serverNum;
 
+        printf("Creating Child PID: %d Name: %s Min Procs:%d Max Procs:%d\n", getpid(), serverName, minProcs, maxProcs);
+        fflush(stdout);
+        displayPrompt();
+
+        //Create children within server
         while(activeProcs < minProcs) {
           if(pipe(fd2[activeProcs]) < 0) {
             perror("pipe error 3");
@@ -99,26 +170,10 @@ int main() {
 
           child[activeProcs] = fork();
 
-          //Child
+          //Child of server
           if(child[activeProcs] == 0) {
-            printf("Creating grandchild for process %d\n", getppid());
             fflush(stdout);
-            while(1) {
-              char fromParent[1024];
-              read(fd2[activeProcs][0], fromParent, sizeof(fromParent));
-
-              printf("Grand child received %s", fromParent);
-              fflush(stdout);
-
-              fromParent[strlen(fromParent) - 1] = '\0';
-
-              if(strstr(fromParent, "kill") != NULL) {
-                printf("Killing grandchild process\n");
-                fflush(stdout);
-                displayPrompt();
-                exit(0);  
-              }
-            }
+            createGrandChild(fd2, activeProcs);
           }
           
           //Parent
@@ -127,47 +182,79 @@ int main() {
           }
         }
 
-        printf("Creating Child PID: %d Name: %s Min Procs:%d Max Procs:%d\n", getpid(), serverName, minProcs, maxProcs);
-        fflush(stdout);
 
         close(fd[child_index][1]);
 
+        //Listen for command
         while(1) {
           char fromPipe[1024];
 
           read(fd[child_index][0], fromPipe, sizeof(fromPipe));
           fromPipe[strlen(fromPipe) - 1] = '\0';
 
-          printf("Child %d read %s from pipe\n",getpid(), fromPipe);
-          fflush(stdout);
-          
           if(strstr(fromPipe, "abortServer") != NULL) {
             char* doNothing = strtok(fromPipe, " ");
             char* serverToAbort = strtok(NULL, " ");
             abortServer(serverName, serverToAbort);
+
           } else if(strstr(fromPipe, "abortProc") != NULL) {
-            printf("got abortProc");
-            fflush(stdout);
             char* doNothing = strtok(fromPipe, " ");
             char* onServer = strtok(NULL, " ");
             abortGrandChild(fd2, activeProcs, minProcs, onServer, serverName);
-          } else if(strstr(fromPipe, "createProc") != NULL) {
-            printf("got createProc\n");
-            fflush(stdout);
 
+          } else if(strstr(fromPipe, "displayStatus") != NULL) {
+            printServerStatus(serverName, getpid(), child, activeProcs); 
+
+          } else if(strstr(fromPipe, "createProc") != NULL) {
+            char* doNothing = strtok(fromPipe, " ");
+            char* onServer = strtok(NULL, " ");
+
+            //Special case: Must create new process without function call :(
+            if((activeProcs + 1) > maxProcs && strcmp(serverName, onServer ) == 0) {
+              printf("Adding another proc would put %s over it's limit of %d procs", serverName, maxProcs);
+              fflush(stdout);
+              displayPrompt();
+            } else {
+              if(pipe(fd2[activeProcs]) < 0) {
+                perror("pipe error 3");
+              }
+
+              child[activeProcs] = fork();
+
+              //Server's new child
+              if(child[activeProcs] == 0) {
+                printf("Creating grandchild for process %d\n", getppid());
+                fflush(stdout);
+                createGrandChild(fd2, activeProcs);
+              }
+
+              if(child[activeProcs]){
+                activeProcs++;
+              }
+            }
           }
         }
         break;
       }
+
       //Parent
       if(server[serverNum]) {
         serverNum++;
       }
+
     } else if(strstr(str, "abortServer") != NULL) {
-      sendServerAbort(fd, str, serverNum);
+      sendCommandToChild(fd, str, serverNum);
       serverNum--;
     } else if(strstr(str, "abortProc") != NULL) {
-      sendServerAbort(fd, str, serverNum); 
+      sendCommandToChild(fd, str, serverNum); 
+    } else if(strstr(str, "createProc") != NULL) {
+      sendCommandToChild(fd, str, serverNum);
+    } else if(strstr(str, "displayStatus") != NULL) {
+      sendCommandToChild(fd, str, serverNum);
+    } else {
+      printf("please enter a valid command\n");
+      fflush(stdout);
+      displayPrompt();
     }
   }
   return 0;
